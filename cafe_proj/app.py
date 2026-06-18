93b92afc730c2073154e797daab2f7a9989f442e
Chaarvi kk <keerthichaarvi24@gmail.com>
2026-06-18 16:55:14 +0530

    connection to customer database

--- cafe_proj/app.py
+++ cafe_proj/app.py
@@ -95,6 +95,54 @@ def remove():
     conn.close()
     return redirect("/cart")
 
+@app.route('/customer')
+def customer():
+    return render_template('customer.html')
 
+@app.route('/place_order', methods=["POST"])
+def place_order():
+
+    conn = get_db_connection()
+    cursor=conn.cursor()
+
+    customer_name=request.form['customer_name']
+    phone_number=request.form['phone_number']
+
+    cursor.execute("""select product_id, quantity from cart""")
+    cart_items=cursor.fetchall()
+
+    total =0
+    for item in cart_items:
+        cursor.execute("select price from products where id=%s",(item[0],))
+
+        price=cursor.fetchone()[0]
+        total+=price*item[1]
+
+    cursor.execute(
+    """
+    INSERT INTO orders(customer_name, phone_number, total)
+    VALUES(%s, %s, %s)
+    """,
+    (customer_name, phone_number, total)
+    )
+
+    order_id=cursor.lastrowid
+    conn.commit()
+
+    for item in cart_items:
+        cursor.execute(
+            """
+            INSERT INTO order_items
+            (order_id, product_id, quantity)
+            VALUES(%s, %s, %s)
+            """,
+            (order_id, item[0], item[1])
+        )
+
+    cursor.execute("DELETE FROM cart")
+
+    conn.commit()
+    conn.close()
+    return "Order placed successfully!" 
 if __name__ == "__main__":
     app.run(debug=True)
